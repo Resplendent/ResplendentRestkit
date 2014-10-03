6f9903d43300e31d853630210a545c67df98cab8
BenMaer <ben@resplendent.co>
2014-10-03 15:47:43 -0700

    - Added RRKAddRouteIfNotAlreadyAdded.  - Added Restkit pod.  - Added RU.

---
+++ ResplendentRestkit/ResplendentRestkit/Code/ResplendentRestkit/Category/RRKAddRouteIfNotAlreadyAdded.h
@@ -0,0 +1,20 @@
+//
+//  RRKAddRouteIfNotAlreadyAdded.h
+//  ResplendentRestkit
+//
+//  Created by Benjamin Maer on 10/3/14.
+//  Copyright (c) 2014 Resplendent. All rights reserved.
+//
+
+#import "RKRouteSet.h"
+
+
+
+
+
+@interface RRKAddRouteIfNotAlreadyAdded : RKRouteSet
+
+-(BOOL)rrk_containsRoute:(RKRoute *)route;
+-(void)rrk_addRouteIfNotAlreadyAdded:(RKRoute *)route;
+
+@end
---
+++ ResplendentRestkit/ResplendentRestkit/Code/ResplendentRestkit/Category/RRKAddRouteIfNotAlreadyAdded.m
@@ -0,0 +1,42 @@
+//
+//  RRKAddRouteIfNotAlreadyAdded.m
+//  ResplendentRestkit
+//
+//  Created by Benjamin Maer on 10/3/14.
+//  Copyright (c) 2014 Resplendent. All rights reserved.
+//
+
+#import "RRKAddRouteIfNotAlreadyAdded.h"
+#import "RUConditionalReturn.h"
+
+
+
+
+
+@implementation RRKAddRouteIfNotAlreadyAdded
+
+-(BOOL)rrk_containsRoute:(RKRoute *)route
+{
+	kRUConditionalReturn_ReturnValueFalse(route == nil, YES);
+	
+	if ([self containsRoute:route])
+	{
+		return YES;
+	}
+	
+	if (route.isNamedRoute && [self routeForName:route.name])
+	{
+		return YES;
+	}
+	
+	return NO;
+}
+
+-(void)rrk_addRouteIfNotAlreadyAdded:(RKRoute *)route
+{
+	kRUConditionalReturn([self rrk_containsRoute:route], NO);
+	
+	[self addRoute:route];
+}
+
+@end
