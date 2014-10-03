5bddf5df56457a5edd4849023618cc34056395f6
BenMaer <ben@resplendent.co>
2014-10-03 15:34:04 -0700

    Added project and code folder.

---
+++ ResplendentRestkit/ResplendentRestkit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ResplendentRestkit
+//
+//  Created by Benjamin Maer on 10/3/14.
+//  Copyright (c) 2014 Resplendent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ResplendentRestkit/ResplendentRestkit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ResplendentRestkit
+//
+//  Created by Benjamin Maer on 10/3/14.
+//  Copyright (c) 2014 Resplendent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
