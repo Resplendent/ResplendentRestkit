4052aad3db1405031964242995ad34e12567655c
BenMaer <ben@resplendent.co>
2014-10-13 19:41:12 -0400

    Messing around with some stuff.

--- ResplendentRestkit/ResplendentRestkit/Code/ResplendentRestkit/Category/RKObjectManager+RRKCreateRequestsFromRoutes.h
+++ ResplendentRestkit/ResplendentRestkit/Code/ResplendentRestkit/Category/RKObjectManager+RRKCreateRequestsFromRoutes.h
@@ -12,6 +12,15 @@
 
 
 
+typedef NS_ENUM(NSInteger, RRKCreateRequestsFromRoutes_RKObjectManager_ImageType) {
+	RRKCreateRequestsFromRoutes_RKObjectManager_ImageType_JPEG,
+	RRKCreateRequestsFromRoutes_RKObjectManager_ImageType_PNG
+};
+
+
+
+
+
 @interface RKObjectManager (RRKCreateRequestsFromRoutes)
 
 //AFNetworking operation
@@ -30,12 +39,47 @@
 															   success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
 															   failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;
 
--(RKObjectRequestOperation*)rrk_enqueueRestkitManagedObjectRequestOperationForRoute:(RKRoute*)route
-																			 object:(NSManagedObject*)object
-																		 parameters:(NSDictionary*)parameters
-																  cancelOldRequests:(BOOL)cancelOldRequests
-															   managedObjectContext:(NSManagedObjectContext *)managedObjectContext
-																			success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
-																			failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;
+//-(RKObjectRequestOperation*)rrk_enqueueRestkitManagedObjectRequestOperationForRoute:(RKRoute*)route
+//																			 object:(NSManagedObject*)object
+//																		 parameters:(NSDictionary*)parameters
+//																  cancelOldRequests:(BOOL)cancelOldRequests
+//																			success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+//																			failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;
+
+//Multipart Form
+-(RKObjectRequestOperation*)rrk_enqueueRestkitManagedObjectMultiPartRequestOperationForMethod:(RKRequestMethod)method
+																						 path:(NSString *)path
+																				   parameters:(NSDictionary *)parameters
+																					   object:(NSManagedObject*)object
+																					formBlock:(void (^)(id <AFMultipartFormData> formData))formBlock
+																			cancelOldRequests:(BOOL)cancelOldRequests
+																		 managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+																					  success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+																					  failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;
+
+-(RKObjectRequestOperation*)rrk_enqueueRestkitManagedObjectMultiPartRequestOperationForMethod:(RKRequestMethod)method
+																						 path:(NSString *)path
+																				   parameters:(NSDictionary *)parameters
+																					   object:(NSManagedObject*)object
+																				   uploadData:(NSData*)uploadData
+																						 name:(NSString*)name
+																					 fileName:(NSString*)fileName
+																					 mimeType:(NSString*)mimeType
+																			cancelOldRequests:(BOOL)cancelOldRequests
+																		 managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+																					  success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+																					  failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;
+
+-(RKObjectRequestOperation*)rrk_enqueueRestkitManagedObjectPNGImageUploadRequestOperationForMethod:(RKRequestMethod)method
+																							  path:(NSString *)path
+																						parameters:(NSDictionary *)parameters
+																						  pngImage:(UIImage*)pngImage
+																							object:(NSManagedObject*)object
+																				 cancelOldRequests:(BOOL)cancelOldRequests
+																			  managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+																						   success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+																						   failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;
+
++(NSString*)mimeTypeForImageType:(RRKCreateRequestsFromRoutes_RKObjectManager_ImageType)imageType;
 
 @end
--- ResplendentRestkit/ResplendentRestkit/Code/ResplendentRestkit/Category/RKObjectManager+RRKCreateRequestsFromRoutes.m
+++ ResplendentRestkit/ResplendentRestkit/Code/ResplendentRestkit/Category/RKObjectManager+RRKCreateRequestsFromRoutes.m
@@ -17,11 +17,11 @@
 
 @interface RKObjectManager (_RRKCreateRequestsFromRoutes)
 
--(NSMutableURLRequest*)rrk_createRestkitAppropriateObjectRequestOperationForRoute:(RKRoute*)route
-																		   object:(id)object
-																	   parameters:(NSDictionary*)parameters
-																		  success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
-																		  failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;
+//-(NSMutableURLRequest*)rrk_createRestkitAppropriateObjectRequestOperationForRoute:(RKRoute*)route
+//																		   object:(id)object
+//																	   parameters:(NSDictionary*)parameters
+//																		  success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+//																		  failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;
 
 /*
  if managedObjectContext param is nil, will return rrk_enqueueRestkitRequestOperationForRoute, otherwise rrk_enqueueRestkitManagedObjectRequestOperationForRoute
@@ -30,9 +30,14 @@
 																				 object:(id)object
 																			 parameters:(NSDictionary*)parameters
 																	  cancelOldRequests:(BOOL)cancelOldRequests
-																   managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+//																   managedObjectContext:(NSManagedObjectContext *)managedObjectContext
 																				success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
 																				failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;
+
+-(RKObjectRequestOperation*)rrk_enqueueRestkitAppropriateObjectRequestOperationForUrlRequest:(NSURLRequest*)urlRequest
+																		managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+																					 success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+																					 failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure;
 
 @end
 
@@ -42,45 +47,57 @@
 
 @implementation RKObjectManager (_RRKCreateRequestsFromRoutes)
 
--(NSMutableURLRequest*)rrk_createRestkitAppropriateObjectRequestOperationForRoute:(RKRoute*)route
-																		   object:(id)object
-																	   parameters:(NSDictionary*)parameters
-																		  success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
-																		  failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure
-{
-	if (route.isNamedRoute)
-	{
-		return [self requestWithPathForRouteNamed:route.name object:object parameters:parameters];
-	}
-	else if (route.isRelationshipRoute)
-	{
-		return [self requestWithPathForRelationship:route.name ofObject:object method:route.method parameters:parameters];
-	}
-	else
-	{
-		NSAssert(false, @"unhandled");
-		return nil;
-	}
-}
+//-(NSMutableURLRequest*)rrk_createRestkitAppropriateObjectRequestOperationForRoute:(RKRoute*)route
+//																		   object:(id)object
+//																	   parameters:(NSDictionary*)parameters
+//																		  success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+//																		  failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure
+//{
+//	if (route.isNamedRoute)
+//	{
+//		return [self requestWithPathForRouteNamed:route.name object:object parameters:parameters];
+//	}
+//	else if (route.isRelationshipRoute)
+//	{
+//		return [self requestWithPathForRelationship:route.name ofObject:object method:route.method parameters:parameters];
+//	}
+//	else
+//	{
+//		NSAssert(false, @"unhandled");
+//		return nil;
+//	}
+//}
 
 -(RKObjectRequestOperation*)rrk_enqueueRestkitAppropriateObjectRequestOperationForRoute:(RKRoute*)route
 																				 object:(id)object
 																			 parameters:(NSDictionary*)parameters
 																	  cancelOldRequests:(BOOL)cancelOldRequests
-																   managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+//																   managedObjectContext:(NSManagedObjectContext *)managedObjectContext
 																				success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
 																				failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure
 {
 	kRUConditionalReturn_ReturnValueNil(route == nil, YES);
 	
 	[self.router.routeSet rrk_addRouteIfNotAlreadyAdded:route];
-
+	
 	if (cancelOldRequests)
 	{
 		[self cancelAllObjectRequestOperationsWithMethod:route.method matchingPathPattern:route.pathPattern];
 	}
 	
-	NSMutableURLRequest* urlRequest = [self rrk_createRestkitAppropriateObjectRequestOperationForRoute:route object:object parameters:parameters success:success failure:failure];
+//	NSMutableURLRequest* urlRequest = [self rrk_createRestkitAppropriateObjectRequestOperationForRoute:route object:object parameters:parameters success:success failure:failure];
+
+//	NSURL *URL = [self.router URLWithRoute:route object:object];
+
+	return [self appropriateObjectRequestOperationWithObject:object method:route.method path:nil parameters:parameters];
+//	return [self rrk_enqueueRestkitAppropriateObjectRequestOperationForUrlRequest:urlRequest managedObjectContext:managedObjectContext success:success failure:failure];
+}
+
+-(RKObjectRequestOperation*)rrk_enqueueRestkitAppropriateObjectRequestOperationForUrlRequest:(NSURLRequest*)urlRequest
+																		managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+																					 success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+																					 failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure
+{
 	kRUConditionalReturn_ReturnValueNil(urlRequest == nil, YES);
 
 	RKObjectRequestOperation* requestOperation = (managedObjectContext ?
@@ -138,20 +155,97 @@
 															   success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
 															   failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure
 {
-	return [self rrk_enqueueRestkitAppropriateObjectRequestOperationForRoute:route object:object parameters:parameters cancelOldRequests:cancelOldRequests managedObjectContext:nil success:success failure:failure];
+	return [self rrk_enqueueRestkitAppropriateObjectRequestOperationForRoute:route object:object parameters:parameters cancelOldRequests:cancelOldRequests success:success failure:failure];
 }
 
--(RKObjectRequestOperation*)rrk_enqueueRestkitManagedObjectRequestOperationForRoute:(RKRoute*)route
-																			 object:(NSManagedObject*)object
-																		 parameters:(NSDictionary*)parameters
-																  cancelOldRequests:(BOOL)cancelOldRequests
-															   managedObjectContext:(NSManagedObjectContext *)managedObjectContext
-																			success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
-																			failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure
-{
-	kRUConditionalReturn_ReturnValueNil(managedObjectContext == nil, YES);
+//-(RKObjectRequestOperation*)rrk_enqueueRestkitManagedObjectRequestOperationForRoute:(RKRoute*)route
+//																			 object:(NSManagedObject*)object
+//																		 parameters:(NSDictionary*)parameters
+//																  cancelOldRequests:(BOOL)cancelOldRequests
+//																			success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+//																			failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure
+//{
+//	kRUConditionalReturn_ReturnValueNil(managedObjectContext == nil, YES);
+//
+//	return [self rrk_enqueueRestkitAppropriateObjectRequestOperationForRoute:route object:object parameters:parameters cancelOldRequests:cancelOldRequests managedObjectContext:managedObjectContext success:success failure:failure];
+//}
 
-	return [self rrk_enqueueRestkitAppropriateObjectRequestOperationForRoute:route object:object parameters:parameters cancelOldRequests:cancelOldRequests managedObjectContext:managedObjectContext success:success failure:failure];
+-(RKObjectRequestOperation*)rrk_enqueueRestkitManagedObjectMultiPartRequestOperationForMethod:(RKRequestMethod)method
+																						 path:(NSString *)path
+																				   parameters:(NSDictionary *)parameters
+																					   object:(NSManagedObject*)object
+																				   uploadData:(NSData*)uploadData
+																						 name:(NSString*)name
+																					 fileName:(NSString*)fileName
+																					 mimeType:(NSString*)mimeType
+																			cancelOldRequests:(BOOL)cancelOldRequests
+																		 managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+																					  success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+																					  failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure
+{
+	return [self rrk_enqueueRestkitManagedObjectMultiPartRequestOperationForMethod:method path:path parameters:parameters object:object formBlock:^(id<AFMultipartFormData> formData) {
+		
+		[formData appendPartWithFileData:uploadData
+									name:name
+								fileName:fileName
+								mimeType:mimeType];
+		
+//		[formData appendPartWithFileData:uploadData
+//									name:@"expense[photo_file]"
+//								fileName:@"photo.jpg"
+//								mimeType:@"image/jpeg"];
+
+	} cancelOldRequests:cancelOldRequests managedObjectContext:managedObjectContext success:success failure:failure];
+}
+
+-(RKObjectRequestOperation*)rrk_enqueueRestkitManagedObjectMultiPartRequestOperationForMethod:(RKRequestMethod)method
+																						 path:(NSString *)path
+																				   parameters:(NSDictionary *)parameters
+																					   object:(NSManagedObject*)object
+																					formBlock:(void (^)(id <AFMultipartFormData> formData))formBlock
+																			cancelOldRequests:(BOOL)cancelOldRequests
+																		 managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+																					  success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+																					  failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure
+{
+	NSMutableURLRequest* urlRequest = [self multipartFormRequestWithObject:object method:method path:path parameters:parameters constructingBodyWithBlock:formBlock];
+
+	return [self rrk_enqueueRestkitAppropriateObjectRequestOperationForUrlRequest:urlRequest managedObjectContext:managedObjectContext success:success failure:failure];
+}
+
+-(RKObjectRequestOperation*)rrk_enqueueRestkitManagedObjectPNGImageUploadRequestOperationForMethod:(RKRequestMethod)method
+																							  path:(NSString *)path
+																						parameters:(NSDictionary *)parameters
+																						  pngImage:(UIImage*)pngImage
+																							object:(NSManagedObject*)object
+																				 cancelOldRequests:(BOOL)cancelOldRequests
+																			  managedObjectContext:(NSManagedObjectContext *)managedObjectContext
+																						   success:(void (^)(RKObjectRequestOperation *operation, RKMappingResult *mappingResult))success
+																						   failure:(void (^)(RKObjectRequestOperation *operation, NSError *error))failure
+{
+	return [self rrk_enqueueRestkitManagedObjectMultiPartRequestOperationForMethod:method path:path parameters:parameters object:object formBlock:^(id<AFMultipartFormData> formData) {
+
+		[formData appendPartWithFileData:UIImagePNGRepresentation(pngImage)
+									name:@"filename"
+								fileName:@"photo.png"
+								mimeType:@"image/png"];
+
+	} cancelOldRequests:cancelOldRequests managedObjectContext:managedObjectContext success:success failure:failure];
+}
+
+#pragma mark - Image Type
++(NSString*)mimeTypeForImageType:(RRKCreateRequestsFromRoutes_RKObjectManager_ImageType)imageType
+{
+	switch (imageType)
+	{
+		case RRKCreateRequestsFromRoutes_RKObjectManager_ImageType_PNG:
+			return @"image/png";
+		case RRKCreateRequestsFromRoutes_RKObjectManager_ImageType_JPEG:
+			return @"image/jpeg";
+	}
+
+	NSAssert(false, @"unhandled");
+	return nil;
 }
 
 @end
